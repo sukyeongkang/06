8cac3b254a25536dc0c3126e9f76110285ccfe6e
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-10-11 16:36:31 +0900

    실습 2

---
+++ main2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void func(void)
+{
+	int x;
+	printf("func x is at %p\n",&x);
+	
+}
+
+int main(void)
+{
+	int x;
+	printf("main x is at %p\n",&x);
+	func();
+	
+	return 0;
+}
