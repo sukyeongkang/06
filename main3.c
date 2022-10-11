d4ee37ce9850a2df8aefb7af93cd9b84936b0e32
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-10-11 21:43:58 +0900

    실습 3

---
+++ main3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+
+
+int sumTwo(int a, int b)
+{
+	int sum = a + b;
+	return(sum);
+}
+
+int square(int n)
+{
+	return(n*n);
+}
+
+int get_max(int x, int y)
+{
+	if (x>y)
+	  return x;
+	else
+	  return y; 
+}
+
+int main()
+{
+
+   int res_sum, res_square, res_get_max;
+                   
+
+   res_sum = sumTwo(2,3);
+   res_square = square(5);
+   res_get_max = get_max(8,7);
+   
+   printf("%i %i %i",res_sum,res_square, res_get_max);
+   return 0;
+}
+   
