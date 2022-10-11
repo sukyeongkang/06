dbc550f78daccfeb9190a256ea3ad8771de9ff1e
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-10-11 16:28:19 +0900

    실습 1

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
 	
+void print_star()
 {
 	int i;
-	
 	for(i=0;i<10;i++)
 	  printf("*"); 
-	for(i=0;i<10;i++)
-	  printf("*"); 
-	for(i=0;i<10;i++)
-	  printf("*"); 
-
-
 }
+
+int main(void)
+{
+	print_star();
+	print_star();
+	print_star();
+
 	return 0;
 }
